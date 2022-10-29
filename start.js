e1d7e544a94c9a7098a9e83fc9a59be02af1be0f
Shandira Ferguson <shandira.ferguson@gmail.com>
2022-10-29 11:15:38 -0700

    Port changed back to 3000

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const server = app.listen(5000, function() {
+const server = app.listen(3000, function() {
     console.log(`Express is running on port ${server.address().port}`);
 });
