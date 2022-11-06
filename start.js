9c2efbc5fd2adfcdf18b863a950f954e6d6728bd
Shandira <shandira.ferguson@gmail.com>
2022-11-05 18:33:54 -0700

    registration schema linked

--- start.js
+++ start.js
@@ -14,7 +14,9 @@ mongoose.connection
         console.log( `Connection error: ${ err.message }` );
     } );
 
+require( './models/Registration' );
 const app = require( './app' );
+
 const server = app.listen( 3000, function () {
     console.log( `Express is running on port ${ server.address().port }` );
 } );
