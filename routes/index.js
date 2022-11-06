aad02cba001abae849ac8d3393632bd9a50831f9
Shandira <shandira.ferguson@gmail.com>
2022-11-05 19:16:53 -0700

    data + formapp database successfully saved + listed on MongoDB Compass

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,41 @@
-const express = require('express');
+const express = require( 'express' );
+const mongoose = require( 'mongoose' );
+const { check, validationResult } = require( 'express-validator' );
+
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const Registration = mongoose.model( 'Registration' );
 
-router.get('/', function(req, res) {
+router.get( '/', function ( req, res ) {
     // res.send('It works!');
-    res.render('form', { title: 'Registration form' });
-});
+    res.render( 'form', { title: 'Registration form' } );
+} );
 
-router.post('/', 
+router.post( '/',
     [
-        check('name')
-            .isLength({ min: 1 })
-            .withMessage('Please enter a name'),
-        check('email')
-            .isLength({ min: 1 })
-            .withMessage('Please enter an email')
-    ], 
-    function(req, res) {
+        check( 'name' )
+            .isLength( { min: 1 } )
+            .withMessage( 'Please enter a name' ),
+        check( 'email' )
+            .isLength( { min: 1 } )
+            .withMessage( 'Please enter an email' )
+    ],
+    function ( req, res ) {
         // console.log(req.body);
-        const errors = validationResult(req);
-        if(errors.isEmpty()) {
-            res.send('Thank you for your registration!');
+        const errors = validationResult( req );
+        if ( errors.isEmpty() ) {
+            const registration = new Registration( req.body );
+            registration.save()
+                .then( () => { res.send( 'Thank you for your registration!' ); } )
+                .catch( ( err ) => {
+                    console.log( err );
+                    res.send( 'Sorry! Something went wrong.' );
+                } )
         } else {
-            res.render('form', { 
+            res.render( 'form', {
                 title: 'Registration form',
                 errors: errors.array(),
-                data: req.body     
-            });
+                data: req.body
+            } );
         }
     }
 );
